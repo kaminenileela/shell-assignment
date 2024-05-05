09b2e03918c9c52c91a913ffe644ad242b762021
kaminenileela <kaminenileela1992@email.com>
2024-05-05 01:29:03 -0400

    count words

--- count.sh
+++ count.sh
@@ -5,8 +5,10 @@ FILE=/Users/leelakamineni/devops/repos/shell-assignment/textfile.txt
 if [ -e $FILE ]
 then
    A=$(cat $FILE | sed -e 's/\.$//g' -e 's/,//g') 
-   echo $A | tr ' ' '\n' | sort -f | uniq -c | sort -nr | head -n 5 | awk -F ' ' '{ print $2, ":", $1}'
+   B=$(echo $A | tr ' ' '\n' | sort -f | uniq -c | sort -nr | head -n 5 | awk -F ' ' '{ print $2, ":", $1}')
 
+   echo " top five most frequent words are below: "
+   
 else
     echo "file does not exist"
 
