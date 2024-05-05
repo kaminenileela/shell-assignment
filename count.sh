60d37b2a74a03e5391d5be7eb3a8a13e53831275
kaminenileela <kaminenileela1992@email.com>
2024-05-05 00:30:54 -0400

    count words

--- count.sh
+++ count.sh
@@ -4,7 +4,9 @@ FILE=/c/Users/Leela/Desktop/devops/daws-78s/repos/shell-assignment/textfile.txt
 
 if [ -e $FILE ]
 then
-   cat $FILE
+   cat $FILE | sed -e 's/\.$//g ; s/\n$//g' >> newfile.txt
+    cat newfile.txt
+
 
 else
     echo "file does not exist"
