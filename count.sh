b61807f272d734f8a6a609efa6fe377247ede0f1
kaminenileela <kaminenileela1992@email.com>
2024-05-05 01:14:21 -0400

    count words

--- count.sh
+++ count.sh
@@ -5,7 +5,7 @@ FILE=/Users/leelakamineni/devops/repos/shell-assignment/textfile.txt
 if [ -e $FILE ]
 then
    A=$(cat $FILE | sed -e 's/\.$//g' | tr -d '\n')
-   echo $A
+   echo $A | sort 
 
 else
     echo "file does not exist"
