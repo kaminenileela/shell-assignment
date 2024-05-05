f0488f65c09200d092ca8862984476f9e04fe99c
kaminenileela <kaminenileela1992@email.com>
2024-05-05 01:26:26 -0400

    count words

--- count.sh
+++ count.sh
@@ -5,7 +5,7 @@ FILE=/Users/leelakamineni/devops/repos/shell-assignment/textfile.txt
 if [ -e $FILE ]
 then
    A=$(cat $FILE | sed -e 's/\.$//g' -e 's/,//g') 
-   echo $A | tr ' ' '\n' | sort -f | uniq -c | sort -nr | head -n 5 | awk -F ' ' '{ print $2 : $1}'
+   echo $A | tr ' ' '\n' | sort -f | uniq -c | sort -nr | head -n 5 | awk -F ' ' '{ print $2, ':' $1}'
 
 else
     echo "file does not exist"
