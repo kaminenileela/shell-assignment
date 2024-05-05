a17563f60d67e6ec15f0f60f4ce67bb542f815b3
kaminenileela <kaminenileela1992@email.com>
2024-05-05 01:29:29 -0400

    count words

--- count.sh
+++ count.sh
@@ -7,7 +7,7 @@ then
    A=$(cat $FILE | sed -e 's/\.$//g' -e 's/,//g') 
    B=$(echo $A | tr ' ' '\n' | sort -f | uniq -c | sort -nr | head -n 5 | awk -F ' ' '{ print $2, ":", $1}')
 
-   echo " top five most frequent words are below: "
+   echo " top five most frequent words are below: $B"
    
 else
     echo "file does not exist"
