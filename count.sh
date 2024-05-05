8452e4ee8d7ab3c16193b237a690614df50472c9
kaminenileela <kaminenileela1992@email.com>
2024-05-05 01:17:12 -0400

    count words

--- count.sh
+++ count.sh
@@ -4,7 +4,7 @@ FILE=/Users/leelakamineni/devops/repos/shell-assignment/textfile.txt
 
 if [ -e $FILE ]
 then
-   A=$(cat $FILE | sed -e 's/\.$//g' | tr -d '\n')
+   A=$(cat $FILE | sed -e 's/\.$//g')
    echo $A | tr ' ' '\n' | sort -i
 
 else
